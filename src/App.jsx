77d728792f65846647f93eee2fa5b0c8d8fcdaab
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-05-26 14:39:59 +0100

    base route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom"
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <>
+      <Nav/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export default function Nav() {
+  return (
+    <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/shop">Shop</Link></li>
+    </ul>
+  </nav>
+  )
+}
