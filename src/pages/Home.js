28271e732c09ef92f1635d1dc76c8aab644ca568
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-05-30 19:10:48 +0100

    lottie player added, lottie for h1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
+import React from "react";
 import star from "../img/icons/star.svg";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import circleLottie from "../img/lottie/circle-lottie.json";
+
 
 export default function Home() {
   return (
@@ -8,12 +12,18 @@ export default function Home() {
         <h1>
           Welcome to Brew<span className="accent">Hive</span>
         </h1>
+        <Player
+          className="circleLottie"
+          autoplay
+          loop
+          src={circleLottie}
+          style={{ height: "300px", width: "300px" }}
+        ></Player>
         <img className="h1-star" src={star} alt="" />
       </div>
 
       <p className="tagline">Unleash Your Coffee Passion with BrewHive</p>
-      <p className="small-txt" >Today I am looking for</p>
+      <p className="small-txt">Today I am looking for</p>
     </div>
   );
 }
-
