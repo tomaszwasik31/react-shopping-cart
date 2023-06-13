89670e2c767f32d0a2666d5dfe8eae0cc09f43eb
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-06-13 17:44:03 +0100

    app base structure rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,7 @@ import Shop from "./pages/Shop";
 import Nav from "./components/Nav";
 import Contact from "./pages/Contact";
 import Stars from "./components/Stars";
-import Machines from "./components/Categories/Machines";
-import Coffee from "./components/Categories/Coffee";
-import TeaSelection from "./components/Categories/TeaSelection";
-import Merchandise from "./components/Categories/Merchandise";
-import GiftSets from "./components/Categories/GiftSets";
-import Subscriptions from "./components/Categories/Subscriptions";
-import Accessories from "./components/Categories/Accessories";
+
 import Basket from "./components/Basket";
 
 function App() {
@@ -56,15 +50,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop/" element={<Shop />}>
-            <Route path="coffee" element={<Coffee />} />
-            <Route path="machines" element={<Machines />} />
-            <Route path="tea-selection" element={<TeaSelection />} />
-            <Route path="merchandise" element={<Merchandise />} />
-            <Route path="gift-sets" element={<GiftSets />} />
-            <Route path="subscriptions" element={<Subscriptions />} />
-            <Route path="accessories" element={<Accessories />} />
-          </Route>
+          <Route path="/shop/" element={<Shop />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,61 +1,100 @@
-import { Outlet, NavLink, useLocation } from "react-router-dom";
 import data from "../data.json";
-import Category from "../components/Category";
 
-const getNavLinkClassName = ({ isActive }) =>
-  isActive ? "btn active-btn" : "btn";
+import React from "react";
 
-const shopLandingPage = (
-  <Category
-    data={data.categories.find(
-      (category) => category.name === "Best Selling Products"
-    )}
-  />
-);
+import coffeeMachine from "../img/coffeeMachine.webp";
+import coffeeBag from "../img/coffeeBag.webp";
+import coffeeAccessories from "../img/coffeeAccessories.webp";
+import gift from "../img/gift.webp";
+import merchandise from "../img/merchandise.webp";
+import subscription from "../img/subscription.webp";
+import tea from "../img/tea.webp";
+
+const categories = [
+  "Coffee",
+  "Tea Selection",
+  "Coffee Merchandise",
+  "Gift Sets",
+  "Coffee Subscriptions",
+  "Coffee Machine",
+  "Coffee Accessories",
+];
 
 export default function Shop() {
-  const location = useLocation();
-  const isShopRoute = location.pathname === "/shop";
+  const [currentCategory, setCurrentCategory] = React.useState("Coffee");
+
+  function changeCategory(name) {
+    setCurrentCategory(name);
+  }
+
+  const currentDataCategory = data.categories.find(
+    (category) => category.name === currentCategory
+  );
+
+  const products = currentDataCategory.items.map((product) => (
+    <div className="product-container" key={product.id} id={product.id}>
+      <div className="small-product-img-container">
+        <img src={getImagePath(product.img)} alt="" className="product-img" />
+        <button className="btn btn-add">Add to basket</button>
+      </div>
+      <div className="product-desc">
+        <p className="product-name">{product.name}</p>
+        <p>Price: ${product.price} </p>
+      </div>
+    </div>
+  ));
+
+  const currentCategoryItems = (
+    <>
+      <div className="category-top-container">
+        <h2 className="category-name">{currentDataCategory.name}</h2>
+        <div className="shop-category-desc">
+          <p>{currentDataCategory.desc}</p>
+        </div>
+      </div>
+      <div className="all-products-container">{products}</div>
+    </>
+  );
 
   return (
     <>
       <div className="page-wrapper container-max">
         <div className="shop-btn-container">
-          <NavLink className={getNavLinkClassName} exact to="/shop/coffee">
-            Coffee
-          </NavLink>
-          <NavLink
-            className={getNavLinkClassName}
-            exact
-            to="/shop/tea-selection"
-          >
-            Tea Selection
-          </NavLink>
-          <NavLink className={getNavLinkClassName} exact to="/shop/merchandise">
-            Coffee Merchandise
-          </NavLink>
-          <NavLink className={getNavLinkClassName} exact to="/shop/gift-sets">
-            Gift Sets
-          </NavLink>
-          <NavLink
-            className={getNavLinkClassName}
-            exact
-            to="/shop/subscriptions"
-          >
-            Coffee Subscriptions
-          </NavLink>
-          <NavLink className={getNavLinkClassName} exact to="/shop/machines">
-            Coffee Machines
-          </NavLink>
-          <NavLink className={getNavLinkClassName} exact to="/shop/accessories">
-            Coffee Accessories
-          </NavLink>
+          {categories.map((category) => (
+            <button
+              key={category}
+              className={`btn ${
+                currentCategory === category ? "active-btn" : ""
+              }`}
+              onClick={() => changeCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
         </div>
-      </div>
-      <div className="page-wrapper container-max">
-        {isShopRoute && shopLandingPage}
-        <Outlet />
+        <div className="shop-category-container">{currentCategoryItems}</div>
       </div>
     </>
   );
+
+  function getImagePath(img) {
+    switch (img) {
+      case "coffeeMachine":
+        return coffeeMachine;
+      case "coffeeBag":
+        return coffeeBag;
+      case "coffeeAccessories":
+        return coffeeAccessories;
+      case "gift":
+        return gift;
+      case "merchandise":
+        return merchandise;
+      case "subscription":
+        return subscription;
+      case "tea":
+        return tea;
+      default:
+        return "";
+    }
+  }
 }
